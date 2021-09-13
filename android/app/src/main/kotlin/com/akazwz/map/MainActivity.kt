e6cbf02ce80dcd8bd659ccaa90b072c9d5fd63f0
akez <1791189210@qq.com>
2021-09-13 16:21:52 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/akazwz/map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akazwz.map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/map.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapPage extends StatefulWidget {
+  MapPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    var markers = <Marker>[
+      Marker(
+        width: 20.0,
+        height: 20.0,
+        point: LatLng(77.855, -62.09),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            textColor: Colors.blue,
+            key: ObjectKey(Colors.blue),
+          ),
+        ),
+      ),
+      Marker(
+        width: 20.0,
+        height: 20.0,
+        point: LatLng(75.455, 2.59),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            textColor: Colors.blue,
+            key: ObjectKey(Colors.blue),
+          ),
+        ),
+      ),
+      Marker(
+        width: 20.0,
+        height: 20.0,
+        point: LatLng(76.255, -136.59),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            textColor: Colors.blue,
+            key: ObjectKey(Colors.blue),
+          ),
+        ),
+      ),
+      Marker(
+        width: 20.0,
+        height: 20.0,
+        point: LatLng(24.555, -16.09),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            textColor: Colors.blue,
+            key: ObjectKey(Colors.blue),
+          ),
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Map')),
+      body: Column(
+        children: [
+          Flexible(
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(70.505, -75.09),
+                zoom: 2,
+                maxZoom: 6,
+                minZoom: 2,
+                bounds: LatLngBounds(LatLng(85.455, 38), LatLng(0, -176.59)),
+                boundsOptions: FitBoundsOptions(padding: EdgeInsets.all(8.0)),
+              ),
+              layers: [
+                TileLayerOptions(
+                  urlTemplate: 'assets/map/zelda/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                MarkerLayerOptions(
+                  markers: markers,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
