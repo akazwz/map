376c7d130ac98247f3e55e85c04d7f7c7aa97b89
akez <1791189210@qq.com>
2021-09-14 15:50:04 +0800

    toast

--- lib/pages/api.dart
+++ lib/pages/api.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:map/src/model/hot_search.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 Future<HotSearch> fetchHotSearch() async {
   final response =
@@ -57,11 +58,15 @@ class _ApiPageState extends State<ApiPage> {
                     child: ListView.builder(
                       itemCount: searches.length,
                       itemBuilder: (context, int index) {
-                        return Column(
-                          children: [
-                            Text(searches[index].topicLead),
-                          ],
-                        );
+                        return ShowSearch(
+                            rank: searches[index].rank,
+                            content: searches[index].content,
+                            hot: searches[index].hot,
+                            topicLead: searches[index].topicLead,
+                            onPress: () {
+                              Fluttertoast.showToast(
+                                  msg: searches[index].content);
+                            });
                       },
                     ),
                   ),
@@ -73,6 +78,35 @@ class _ApiPageState extends State<ApiPage> {
             return const CircularProgressIndicator();
           },
         ),
+      ),
+    );
+  }
+}
+
+class ShowSearch extends StatelessWidget {
+  ShowSearch({
+    @required this.rank,
+    @required this.content,
+    @required this.hot,
+    @required this.topicLead,
+    @required this.onPress,
+  });
+
+  final int rank;
+  final String content;
+  final int hot;
+  final String topicLead;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Text(rank.toString() + " : " + hot.toString()),
+          TextButton(onPressed: this.onPress, child: Text(content)),
+          Text(topicLead),
+        ],
       ),
     );
   }
